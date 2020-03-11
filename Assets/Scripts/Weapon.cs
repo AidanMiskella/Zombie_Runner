36a5dabc6639e36959412b943312cd9bfe7e49c0
Aidan Miskella <aidanmiskella10@gmail.com>
2020-03-11 11:52:26 +0000

    import guns

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -11,23 +11,29 @@ public class Weapon : MonoBehaviour {
     [SerializeField] ParticleSystem muzzleFlash;
     [SerializeField] GameObject hitEffect;
     [SerializeField] Ammo ammoSolt;
+    [SerializeField] float timeBetweenShots = 0.5f;
+
+    bool canShoot = true;
 
     void Update() {
 
-        if (Input.GetButtonDown("Fire1")) {
+        if (Input.GetMouseButtonDown(0) && canShoot == true) {
 
-            Shoot();
+            StartCoroutine(Shoot());
         }
     }
 
-    private void Shoot() {
+    IEnumerator Shoot() {
 
+        canShoot = false;
         if (ammoSolt.GetCurrentAmmo() > 0) {
 
             PlayMuzzleFlash();
             ProcessRaycast();
             ammoSolt.ReduceCurrentAmmo();
         }
+        yield return new WaitForSeconds(timeBetweenShots);
+        canShoot = true;
     }
 
     private void PlayMuzzleFlash() {
